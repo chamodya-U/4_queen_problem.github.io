8f2179c4f7f9a1fdcfca09a0aefed6dfa7052f74
Chamodya <chamodyaUmayanga123@gmail.com>
2026-06-09 23:13:52 +0530

    finally

--- script.js
+++ script.js
@@ -9,15 +9,7 @@ ctx.fillStyle = "blue";
 let l = 80;
 let gap = 2;
 
-let drawRect = (item) => ctx.fillRect(item.x1, item.y1, item.x2, item.y2);;
-
-
-//draw board
-iterateMatrix(cordinates, drawRect);
-
-
-
-
+//Create cordinates
 for (let i = 0; i < 4; i++) {
   let row = [];
   for (let j = 0; j < 4; j++) {
@@ -36,42 +28,101 @@ for (let i = 0; i < 4; i++) {
       x2: x2,
       y2: y2,
     });
-    cordinates.push(row);
   }
+  cordinates.push(row);
 }
 //console.log(cordinates[0][0]);
+
+//draw board
+function drawboard(arr) {
+  let rowlen = arr.length;
+  let collen = arr[0].length;
+
+  for (let i = 0; i < rowlen; i++) {
+    for (let j = 0; j < collen; j++) {
+      let item = arr[i][j];
+      ctx.fillRect(item.x1, item.y1, item.x2, item.y2);
+    }
+  }
+}
+drawboard(cordinates);
 
 const canvasStart = canvas.getBoundingClientRect();
 const canvasStartX = canvasStart.left;
 const canvasStartY = canvasStart.top;
 
-cordinates.forEach((item) => {
-  ctx.fillRect(item.x1, item.y1, item.x2, item.y2);
-});
+function boudaryCheck(b1, b2, value) {
+  if (value >= b1 && value <= b2) return true;
+}
 
-/* canvas.addEventListener("mousemove", (e) => {
-  console.log(e.clientX, e.clientY);
+function drawCircle(x, y, r) {
+  ctx.beginPath();
+  ctx.arc(x, y, r, 0, 2 * Math.PI);
+  ctx.stroke();
+  ctx.fillStyle = "black";
+  ctx.fill();
+}
 
-  const rect = canvas.getBoundingClientRect();
-  console.log(rect);
-
-  const x = e.clientX - rect.left;
-  const y = e.clientY - rect.top;
-
-  cordinates.forEach((cell) => {
-    if (
-      x >= cell.x1 &&
-      x <= cell.x1 + cell.x2 &&
-      y >= cell.y1 &&
-      y <= cell.y1 + cell.y2
-    ) {
-      ctx.fillStyle = "red";
-      ctx.fillRect(cell.x1, cell.y1, cell.x2, cell.y2);
-    }
-  });
-});
-*/
 //console.log(cordinates);
+
+//c stqand for codinate
+function showpath(c, arr, i, j) {
+  let shouldCheckX = c.x1;
+  let shouldCheckY = c.y1;
+  ctx.fillStyle = "red";
+
+  arr.forEach((row) => {
+    row.forEach((item) => {
+      if (item.x1 == shouldCheckX) {
+        drawCircle(item.x1 + item.x2 / 2, item.y1 + item.y2 / 2, 5);
+      }
+      if (item.y1 == shouldCheckY) {
+        drawCircle(item.x1 + item.x2 / 2, item.y1 + item.y2 / 2, 5);
+      }
+    });
+  });
+
+  //make diagonal
+  let d = [];
+  let k = i;
+  let l = j;
+
+  while (k - 1 >= 0 && l - 1 >= 0) {
+    k--;
+    l--;
+    d.push([k, l]);
+  }
+  k = i;
+  l = j;
+
+  while (k + 1 < 4 && l + 1 < 4) {
+    k++;
+    l++;
+    d.push([k, l]);
+  }
+
+  k = i;
+  l = j;
+
+  while (k - 1 >= 0 && l + 1 < 4) {
+    k--;
+    l++;
+    d.push([k, l]);
+  }
+
+  while (l - 1 >= 0 && k + 1 < 4) {
+    k++;
+    l--;
+    d.push([k, l]);
+  }
+
+  //console.log(arr[0]);
+
+  d.forEach((index) => {
+    item = arr[index[0]][index[1]];
+    drawCircle(item.x1 + item.x2 / 2, item.y1 + item.y2 / 2, 5);
+  });
+}
 
 window.addEventListener("click", (e) => {
   let x = e.clientX;
@@ -91,74 +142,19 @@ window.addEventListener("click", (e) => {
   }
 
   if (useableX && useableY) {
-    console.log(useableX, useableY);
-    cordinates.forEach((item) => {
-      let checkX = boudaryCheck(item.x1, item.x1 + item.x2, useableX);
-      let checkY = boudaryCheck(item.y1, item.y1 + item.y2, useableY);
-      //console.log(checkX, checkY);
+    cordinates.forEach((row, i) => {
+      row.forEach((item, j) => {
+        let checkX = boudaryCheck(item.x1, item.x1 + item.x2, useableX);
+        let checkY = boudaryCheck(item.y1, item.y1 + item.y2, useableY);
+        //console.log(checkX, checkY);
 
-      if (checkX && checkY) {
-        showpath(item, cordinates);
-        drawCircle(item.x1 + item.x2 / 2, item.y1 + item.y2 / 2, 15);
-      }
+        if (checkX && checkY) {
+          showpath(item, cordinates, i, j);
+          drawCircle(item.x1 + item.x2 / 2, item.y1 + item.y2 / 2, 15);
+        }
+      });
     });
   }
 
   //console.log(e.clientX, e.clientY);
 });
-
-function iterateMatrix(arr, func) {
-  let rowlen = arr.length;
-  let collen = arr[0].length;
-  
-  for (let i = 0; i < rowlen; i++) {
-    for (let j = 0; j < collen; j++) {
-      func(arr[i][j]);
-      
-    }
-    
-  }
-}
-
-function boudaryCheck(b1, b2, value) {
-  if (value >= b1 && value <= b2) return true;
-}
-
-function drawCircle(x, y, r) {
-  ctx.beginPath();
-  ctx.arc(x, y, r, 0, 2 * Math.PI);
-  ctx.stroke();
-  ctx.fillStyle = "black";
-  ctx.fill();
-}
-
-//c stqand for codinate
-function showpath(c, arr) {
-  let shouldCheckX = c.x1;
-  let shouldCheckY = c.y1;
-  ctx.fillStyle = "red";
-  let lineX = [];
-  let lineY = [];
-  arr.forEach((item) => {
-    if (item.x1 == shouldCheckX) {
-      //ctx.fillRect(item.x1, item.y1, item.x2, item.y2);
-      drawCircle(item.x1 + item.x2 / 2, item.y1 + item.y2 / 2, 5);
-      lineX.push(item);
-    }
-    if (item.y1 == shouldCheckY) {
-      //ctx.fillRect(item.x1, item.y1, item.x2, item.y2);
-      drawCircle(item.x1 + item.x2 / 2, item.y1 + item.y2 / 2, 5);
-      lineY.push(lineY);
-    }
-
-    //make diagonal
-    lineX.forEach((itemX) => {
-      lineY.forEach((itemY) => {
-        if (itemX.x1 == itemY.y1 && itemX.y1 == itemY.x1) {
-          drawCircle(item.x1 + item.x2 / 2, item.y1 + item.y2 / 2, 5);
-          //console.log(true);
-        }
-      });
-    });
-  });
-}
